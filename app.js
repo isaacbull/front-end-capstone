cf11bebe8f7872ebfbaedf620cb2a3c22f99210e
SmoothAI <117191513+isaacbull@users.noreply.github.com>
2023-08-14 16:56:57 -0400

    Create app.js
    adding react components

---
+++ app.js
@@ -0,0 +1,17 @@
+const data = [
+  {
+    id: "1",
+    title: "Health Diagnosis and Prognosis",
+    description: "Use Machine Learning to diagnose your medical images and create prognosis for your patients",
+    image: "https://th.bing.com/th/id/R.29cb672050d885ca3287190f2edf6b79?rik=Ne7T4UpfnxNeBQ&riu=http%3a%2f%2fhtml.scirp.org%2ffile%2f14-1560633x2.png&ehk=FA8%2fC%2b2fibv%2fm98k%2btp%2fvfdeLlw29%2f%2fvaPMQN%2bAJIaw%3d&risl=&pid=ImgRaw&r=0",
+    price: "$55.00/m",
+  },
+];
+
+
+})
+
+export default function App() {
+  console.log(data);
+  return <h1>Examine the console output</h1>;
+}
